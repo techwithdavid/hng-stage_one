1964631a87913195545ab558349dbc52e0ea68a9
David Fabusuyi <daveed925@gmail.com>
2023-09-08 12:49:04 -0700

    Updated the file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 
 app = Flask(__name__)
 
-app.route('/api')
+app.route('/api', methods=['GET'])
 def details():
     slack_name = requests.args.get('slack_name')
     track = requests.args.get('track')
@@ -15,9 +15,9 @@ def details():
         'current_day': current_day,
         'utc_time': current_time,
         'track': track,
-        'github_file_url':,
-        'github_repo_url':,
-        'status_code':
+        'github_file_url': "https://github.com/techwithdavid/h    ng-stage_one/blob/master/main.py",
+        'github_repo_url': "https://github.com/techwithdavid/h    ng-stage_one",
+        'status_code': 200:
         }), 200
 
 if __name__=='__main__':
