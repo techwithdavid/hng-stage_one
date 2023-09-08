7fc52833aa8f7452fc3807d61915c4c4f1900b9e
David Fabusuyi <daveed925@gmail.com>
2023-09-08 12:36:52 -0700

    Added the file

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import datetime
+
+app = Flask(__name__)
+
+app.route('/api')
+def details():
+    slack_name = requests.args.get('slack_name')
+    track = requests.args.get('track')
+    current_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    current_day = datetime.datetime.now().strftime("%A")
+
+    return jsonify({
+        'slack_name': slack_name,
+        'current_day': current_day,
+        'utc_time': current_time,
+        'track': track,
+        'github_file_url':,
+        'github_repo_url':,
+        'status_code':
+        }), 200
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
